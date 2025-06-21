936f40fd12249d19ffcf50aa33dcad879b12ebd7
Liana Grigoryan <lianagrigoryan.1995.lg@gmail.com>
2025-06-21 21:07:08 +0400

    backend port

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -50,6 +50,14 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    description = "App"
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
